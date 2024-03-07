404f2bb8c3f8087537b125ccf6e17147ad7a6234
MinhHieu-Nguyen-dn <mhieu.forwork@gmail.com>
2024-03-08 01:37:59 +0700

    98 version modifications

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ import os
 import time
 
 # Set the theme colors
-st.set_page_config(page_title="Tucano International Women Day", page_icon="logo.png", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Kễnh con 98", layout="wide", initial_sidebar_state="expanded")
 
 # Display the title and logo in the same row with adjusted column sizes
 col1, col2 = st.columns([1, 3])
 with col1:
-    st.image("logo.png", width=50)  # Adjust the width to your preference
+    st.image("logo.jpg", width=50)  # Adjust the width to your preference
 with col2:
-    st.title("Nhân ngày Quốc tế Phụ nữ, ae Tucano boys chúc các chị em")
+    st.title("Nhân ngày Quốc tế Phụ nữ, chúc các em gái 98")
 
 # Split the web into 2 columns with equal width
 col3, col4, col5 = st.columns(3)
 
 # Get the list of images
-image_list = os.listdir("pics")
+image_list = os.listdir("pics98")
 
 # Display the wishes in the second column
 with open("wishes.txt", "r", encoding='utf-8') as file:
@@ -34,8 +34,8 @@ while True:
         wishes_placeholder.write(wish)
 
         # Display the images in order
-        img_1 = col3.image(f"pics/{image_list[wish_counter % len(image_list)]}", width=200)  # Adjust the width to your preference
-        img_2 = col5.image(f"pics/{image_list[(wish_counter + 1) % len(image_list)]}", width=200)
+        img_1 = col3.image(f"pics98/{image_list[wish_counter % len(image_list)]}", width=200)  # Adjust the width to your preference
+        img_2 = col5.image(f"pics98/{image_list[(wish_counter + 1) % len(image_list)]}", width=200)
 
         # Sleep for a while
         time.sleep(1)
